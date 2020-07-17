ba05aaa11bfcce11b33df82d208155643edde7a3
“Enter <ashwinigirase26@gmail.com>
2020-07-17 21:15:26 +0530

    REFACTOR:code to calculate wages till condition of total working hours.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -104,3 +104,51 @@ public class EmployeeWageComputation {
 	}
 		System.out.println("Total Wages in a month = "+totalWages);
 } }
+
+public class EmpWageComputation {
+
+	final static int WAGE_PER_HOUR=20; //Assigned wage per hour with 20
+	final static int FULL_TIME=2; //Assigned full time with 2
+	final static int PART_TIME=1; //Assigned part time with 1
+	final static int MAX_WORKING_DAYS=20; //Assigned maximum working days in month with 20
+	final static int MAX_WORKING_HOURS=100; //Assigned maximum working hours in month with 100
+	static int dailyWage;	 //Stores dailyWage of employee
+	static int workHours;  //Stores work hour of employee
+	static int totalWorkingDays=1; //Stores total working days of an employee in month
+	static int totalWages=0; //Stores total wages in a month
+	static int totalWorkingHrs=1; //Stores total working hours of an employee in month
+	
+	//Computes DailWage of an Employee
+	public static int ComputeDailyWage(int hours) {
+		return hours*WAGE_PER_HOUR;
+	} 
+	public static void main(String[] args) {
+		int attendence; //Stores value of employee attendance 
+		
+		//Continues till totalWorkings days reached maximum working days
+		while(totalWorkingDays<=MAX_WORKING_DAYS || totalWorkingHrs<=MAX_WORKING_DAYS) {
+			attendence=(int)(Math.random()*3); // Generates 0,1 or 2 using random() and stores the value
+			
+			/*Checks if Employee is working full time,part time or absent and store work hours and calculate daily wage of employee
+		  	according to work time*/
+			switch(attendence) {
+				case FULL_TIME: workHours=8;
+								dailyWage=ComputeDailyWage(workHours);
+								break;
+						
+				case PART_TIME: workHours=4;
+								dailyWage=ComputeDailyWage(workHours);
+								break;
+						
+				default:		workHours=0;
+		
+		}
+		
+		System.out.println("Daily Wage of an Employee on Day "+totalWorkingDays+" is "+dailyWage);
+		totalWorkingHrs+=workHours; //Increments by workHours
+		totalWages+=dailyWage; //sum of daily wages in month
+		dailyWage=0; // Assigns daily wage to 0 for the purpose of next iteration
+		totalWorkingDays++;
+	}
+		System.out.println("Total Wages in a month = "+totalWages);
+} }
